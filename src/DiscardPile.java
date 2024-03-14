e4d27da1452856ed0c0b5c7364dece2a673c0fae
TrainFan1804 <oleschulz123@gmail.com>
2024-03-14 18:25:53 +0100

    Work on the pile class

--- src/DiscardPile.java
+++ src/DiscardPile.java
@@ -2,6 +2,12 @@
  * @author g.ary, o.le
  * @since 13.03.2024
  */
-public class DiscardPile {
+public class DiscardPile extends Pile{
+
+    @Override
+    protected boolean addCard(Card newCard) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'addCard'");
+    }
     
 }
--- src/DrawPile.java
+++ src/DrawPile.java
@@ -2,6 +2,12 @@
  * @author g.ary, o.le
  * @since 13.03.2024
  */
-public class DrawPile {
+public class DrawPile extends Pile {
+
+    @Override
+    protected boolean addCard(Card newCard) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'addCard'");
+    }
     
 }
