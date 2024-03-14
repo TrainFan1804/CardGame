48a8a1dc9b3438b073188ac65a8ddac12eca0820
TrainFan1804 <oleschulz123@gmail.com>
2024-03-14 18:18:36 +0100

    Work on Player

--- src/Player.java
+++ src/Player.java
@@ -4,4 +4,27 @@
  */
 public class Player {
     
+    public Player() {
+
+
+    }
+
+    public int shop(Game g) {
+
+        return 0;
+    }
+
+    public void playCard(Game g) {
+
+    }
+
+    public void end(double finalScore) {
+
+
+    }
+
+    public void run(Game g) {
+
+    }
+
 }
