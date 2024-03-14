19380bd974c03b52ae892482025a05322f063546
TrainFan1804 <oleschulz123@gmail.com>
2024-03-14 18:24:50 +0100

    Fixed suboptimal code

--- src/Card.java
+++ src/Card.java
@@ -4,33 +4,38 @@
  */
 public class Card {
 
-    private Shape Shape;
+    private Shape shape;
     private double point;
 
     public Card(Shape shape, double point) {
 
-        this.Shape = shape;
+        this.shape = shape;
         this.point = point;
 
     }
 
     public Shape getShape() {
-        return Shape;
+
+        return this.shape;
     }
 
     public void setShape(Shape shape) {
-        Shape = shape;
+
+        this.shape = shape;
     }
 
     public double getPoint() {
-        return point;
+
+        return this.point;
     }
 
     public void setPoint(double point) {
+
         this.point = point;
     }
 
     public String getName() {
+        
         return null;
     }
     
