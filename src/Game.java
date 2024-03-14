5d312ead577df5d061cd82cb1a5931a638eb4592
TrainFan1804 <oleschulz123@gmail.com>
2024-03-14 18:35:15 +0100

    Work on Game.java

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,79 @@
  * @since 13.03.2024
  */
 public class Game {
+
     public Game(int maxRounds) {
         
     }
+
+    public double getCredits() {
+
+        return 1.0;
+    }
+
+    public Mode getMode() {
+
+        return null;
+    }
+
+    public int getStackCound() {
+
+        return 0;
+    }
+
+    public boolean isShopEmpty() {
+
+        return false;
+    }
+
+    public int getShopItemCount() {
+
+        return 0;
+    }
+
+    public String getShopItemDescription(int shopItemIndex) {
+
+        return null;
+    }
+
+    public int getShopItemPrice(int shopItemIndex) {
+
+        return 0;
+    }
+
+    public boolean buy(int shopItemIndex) {
+
+        return false;
+    }
+
+    public boolean isDrawPileEmpty() {
+
+        return false;
+    }
+
+    public void endShopping() {
+
+
+    }
+
+    public int getHandCardsCound() {
+
+        return 0;
+    }
+
+    public Card getHandCard(int handCardIndex) {
+
+        return null;
+    }
+
+    public void play(Card card, int stackIndex) {
+
+        
+    }
+
+    public Shape[] getTopShapes() {
+
+        return null;
+    }
+    
 }
